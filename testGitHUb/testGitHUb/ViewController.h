47819405d70b1385d836c0b4e6608740574c65f1
赖伟煌 <laiweihuang126@qq.com>
2015-07-09 13:16:26 +0800

    1、Test

---
+++ testGitHUb/testGitHUb/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testGitHUb
+//
+//  Created by lai on 15/7/9.
+//  Copyright (c) 2015年 laiweihuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testGitHUb/testGitHUb/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  testGitHUb
+//
+//  Created by lai on 15/7/9.
+//  Copyright (c) 2015年 laiweihuang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"laiweihuangTest1");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
