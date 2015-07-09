6194cb51b14671d4fc25c50ca04c82ea337322a8
赖伟煌 <laiweihuang126@qq.com>
2015-07-09 13:26:05 +0800

    1、测试

--- testGitHUb/testGitHUb/ViewController.m
+++ testGitHUb/testGitHUb/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"laiweihuangTest1");
+    NSLog(@"赖伟煌Test!");
 }
 
 - (void)didReceiveMemoryWarning {
